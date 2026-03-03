65675fe23cbe0556c2dca93f67793bf2497bc0e9
Maxine Zick <maxine@pnk.dev>
2026-03-02 22:29:45 -0500

    fix infinite recursion bug when using the overlays module

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -1,41 +1,23 @@
-{ config, lib, ... }:
+{ inputs, config, lib, ... }:
 let
   inherit (lib) mkOption types;
 
-  packageSetEntry = types.either types.package packageSet;
-  packageSet = types.attrsOf packageSetEntry;
-
-  importable =
-    types.either
-      types.path
-      ( types.addCheck
-          (builtins.hasAttr "outPath")
-          (types.attrsOf types.unspecified)
-      );
-
-  importableAs = types.coercedTo importable (
-    overlay:
-      assert (builtins.isAttrs overlay) -> (overlay ? outPath);
-
-      builtins.import (
-        if    builtins.isAttrs overlay
-        then  overlay.outPath
-        else  overlay
-      )
-  );
-
-  overlay = types.functionTo (types.functionTo (types.attrsOf packageSetEntry));
+  overlay = types.unspecified;
 in
 {
   options.overlays = mkOption {
-    type = types.listOf (importableAs overlay);
-    default = [];
+    type = types.attrsOf (types.listOf overlay);
+    default = {};
 
     description = ''
-      A list of overlays to apply to the `nixpkgs` flake input, if present.
+      An attribute set of overlays that can be applied as needed.
 
-      The attribute set produced by the overlay is expected to have entries
-      that are either packages or package sets.
+      By default, `overlays.nixpkgs = [...];` will be applied to the
+      `nixpkgs` flake input, if present. This is done as a special module
+      argument for `pkgs` (`_module.args.pkgs = ...`).
+
+      The attribute set produced by each overlay is expected to have
+      entries that are either packages or package sets.
 
       Package sets are produced with
       ```nix
@@ -48,18 +30,15 @@ in
           # OR nested package sets.
         })
       ```
-
-      A path that points to a nix file containing the overlay is equally valid.
-
-      An attribute set with `outPath` that points to a nix file
-      containing the overlay is equally valid.
     '';
   };
 
-  config.perSystem = { system, inputs', ... }: {
-    _module.args.pkgs = import inputs'.nixpkgs {
-      inherit system;
-      inherit (config) overlays;
+  config = lib.mkIf (inputs ? nixpkgs) {
+    perSystem = { system, ... }: {
+      _module.args.pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = config.overlays.nixpkgs or [];
+      };
     };
   };
 }
